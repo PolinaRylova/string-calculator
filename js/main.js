9709bf9762fbfa9893e6938ff3b8a44ffaf8339a
Polina Rylova <mail@polinarya.ru>
2017-12-27 01:15:06 +0500

    Переписала логику (нужно отловить ошибку добавления в начало массива operandsAndLowPriorityOperators пустой строки

--- js/main.js
+++ js/main.js
@@ -7,66 +7,138 @@
     return string.split('');
   };
 
+  const areBadNeighbors = function (item, currentIndex, previousIndex) {
+    return (item === '*' || item === '/') && (currentIndex - previousIndex === 1);
+  };
+
   const parseExpressions = function (array) {
     let parsedArray = [];
-    let operand = "";
-    array.forEach(function (item, index, array) {
-      if (availableOperands.indexOf(item) >= 0) {
-        operand += item;
-      } else if (availableOperators.indexOf(item) >= 0) {
-        // если юзер первым символом ввел оператор, а не операнд
-        if (operand === "") {
-          parsedArray.push("0");
-        } else {
-          parsedArray.push(operand);
-          operand = "";
+    let operand = '';
+    let prevIndex;
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0 && (array[i] === '*' || array[i] === '/')) {
+        throw new Error('Ошибка ввода. Первыми символами не могут быть "*" и "/".');
+      }
+      if (availableOperands.indexOf(array[i]) >= 0) {
+        operand += array[i];
+      } else if (availableOperators.indexOf(array[i]) >= 0) {
+        if (array[i] === '+' || array[i] === '-') {
+          prevIndex = i;
+        } else if (areBadNeighbors(array[i], i, prevIndex)) {
+          throw new Error('Ошибка ввода. Не поддерживаются сочетания "+*", "-*, "+/", "-/".');
         }
-      // кладем в массив оператор (всегда следующим за операндом)
-      parsedArray.push(item);
+        parsedArray.push(operand); // кладем в массив хранящееся в операнде значение
+        operand = ''; // очищаем переменную операнда
+        parsedArray.push(array[i]); // кладем в массив оператор
       }
-      // добавляем в массив последний из операндов
-      if (index === array.length - 1) {
-        parsedArray.push(operand);
+      if (i === array.length - 1) {
+        if (availableOperators.indexOf(array[i]) >= 0) { // проверяем на ошибки ввода
+          throw new Error('Ошибка ввода. В конце выражения не должно быть знаков - только цифры.');
+        } else {
+          parsedArray.push(operand); // добавляем в массив последний из операндов
+        }
       }
-    });
-    /*
-    Если юзер ввел пустую строку, неподдерживаемый символ или ничего не ввел
-    и нажал "Посчитать", то вернётся пустой массив
-    */
-    return parsedArray;
+    }
+    if (parsedArray.length > 0) {
+      console.log('parsedArray равен ' + parsedArray);
+      return parsedArray;
+    } else {
+      throw new Error('Ошибка ввода. Вы ввели неподдерживаемый символ или отправили пустую строку.');
+    }
   };
 
-  const calculateExpression = function (string) {
-    return eval(string);
+  const calculateOperands = function (previousOperand, operator, currentOperand) {
+    let result;
+    switch (operator) {
+      case '*':
+        result = previousOperand * currentOperand;
+        break;
+      case  '/':
+        result = previousOperand / currentOperand;
+        break;
+      case '+':
+        result = previousOperand + currentOperand;
+        break;
+      case  '-':
+        result = previousOperand - currentOperand;
+        break;
+    }
+    console.log('result равен ' + result);
+    return result;
   };
 
-  const showToDiv = function (value, selector) {
+  const transformToLowPriority = function (array) {
+    let previousOperand = '';
+    let currentOperand = '';
+    let operator = '';
+    let operandsAndLowPriorityOperators = [];
+    for (let i = 0; i < array.length; i++) {
+      if (availableOperators.indexOf(array[i])) { // если текущий элемент является оператором
+        if (array[i] === '*' || array[i] === '/') {
+          operator = array[i]; // сохраняем оператор для операции * или /
+        } else {
+          operandsAndLowPriorityOperators.push(previousOperand); // пушим предыдущий операнд в новый массив
+          operandsAndLowPriorityOperators.push(array[i]); // пушим оператор в новый массив
+          previousOperand = ''; // обнуляем предыдущий операнд для следующей операции
+          operator = ''; // очищаем оператор для следующей операции
+        }
+      } else { // если текущий элемент является операндом
+        currentOperand = array[i];
+        if (operator) { // если оператор сохранён, вычисляем произведение или разницу
+          previousOperand = calculateOperands(previousOperand, operator, currentOperand);
+        } else { // если нет сохранённого оператора, то сохраняем текущее значение в предыдущий операнд
+          previousOperand = currentOperand;
+        }
+      }
+    }
+    console.log('operandsAndLowPriorityOperators равен ' + operandsAndLowPriorityOperators);
+    return operandsAndLowPriorityOperators;
+  };
+
+  const calculateResult = function (array) {
+    let operandsAndLowPriorityOperators = transformToLowPriority(array);
+    let currentOperand = '';
+    let previousOperand = '';
+    let operator = '';
+    for (let i = 0; i < operandsAndLowPriorityOperators.length; i++) {
+      if (availableOperators.indexOf(array[i])) {
+        if (operator) {
+          previousOperand = calculateOperands(previousOperand, operator, currentOperand);
+        }
+        operator = array[i];
+      } else {
+        if (!operator) {
+          previousOperand = array[i];
+        } else {
+          currentOperand = array[i];
+        }
+      }
+    }
+    console.log('previousOperand равен ' + previousOperand);
+    return previousOperand;
+  };
+
+  const showResult = function (value) {
     const text = "Ваш результат равен ";
-    document.querySelector(selector).textContent = text + value;
+    document.querySelector('.result-block').textContent = text + value;
   };
 
   const calculateInputtedExpression = function () {
-    // сохраняем введенную строку
-    let taskField = document.querySelector('.task-field');
-    // вызываем функцию, преобразующую строку в массив, и сохраняем результат в переменной
-    const inputtedExpressions = splitToArray(taskField.value);
-    // разбираем массив введенных значений на операторы и операнды и складываем в новый массив
-    const parsedMembers = parseExpressions (inputtedExpressions);
-    /*
-    Проверяем, заполнен ли массив: если заполнен, делаем вычисления,
-    если пустой - выводим ошибку и очищаем поле ввода
-     */
-    if (parsedMembers.length > 0) {
-      // преобразуем массив в строку
-      let stringOfExpression = parsedMembers.join(" ");
-      // вызываем функцию, вычисляющую из строки результат
-      const result = calculateExpression(stringOfExpression);
+    let taskField = document.querySelector('.task-field'); // находим поле для ввода выражения юзером
+    const inputtedExpressions = splitToArray(taskField.value); // преобразуем строковое значение поля в массив и сохраняем в переменную
+    taskField.value = ''; // очищаем поле для ввода выражения
+    console.log('длина первоначального массива' + inputtedExpressions.length);
+    try {
+      // разбираем массив введенных значений на операторы и операнды и складываем в новый массив
+      const parsedMembers = parseExpressions (inputtedExpressions);
+      console.log('длина разобранного на члены массива' + parsedMembers.length);
+      // вызываем функцию, вычисляющую из массива результат
+      const result = calculateResult(parsedMembers);
       // вызываем функцию, отображающюю результат
-      showToDiv(result, '.result-block');
-    } else {
-      alert('Ошибка ввода. Вы можете вводить цифры или знаки "+", "-", "*", "/".');
+      showResult(result);
+    } catch (e) {
+      alert(e.message);
     }
-    taskField.value = "";
   };
 
   document.querySelector('.calculate-btn').addEventListener('click', calculateInputtedExpression);
