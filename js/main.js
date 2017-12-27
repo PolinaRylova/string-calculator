9b8afe9746f1c6942d01ad1b5ba704a83abd08a4
Polina Rylova <mail@polinarya.ru>
2017-12-27 20:30:06 +0500

    Переписала логику и поправила баги

--- js/main.js
+++ js/main.js
@@ -7,8 +7,16 @@
     return string.split('');
   };
 
+  const isHigherPrecedence = function (item) {
+    return (item === '*' || item === '/');
+  };
+
+  const isLowerPrecedence = function (item) {
+    return (item === '+' || item === '-');
+  };
+
   const areBadNeighbors = function (item, currentIndex, previousIndex) {
-    return (item === '*' || item === '/') && (currentIndex - previousIndex === 1);
+    return (isHigherPrecedence(item)) && (currentIndex - previousIndex === 1);
   };
 
   const parseExpressions = function (array) {
@@ -16,31 +24,32 @@
     let operand = '';
     let prevIndex;
     for (let i = 0; i < array.length; i++) {
-      if (i === 0 && (array[i] === '*' || array[i] === '/')) {
+      // проверяем первый введенный символ на соответствие * или /
+      if (i === 0 && (isHigherPrecedence(array[i]))) {
         throw new Error('Ошибка ввода. Первыми символами не могут быть "*" и "/".');
       }
+      // если введен операнд (число), сохраняем его
       if (availableOperands.indexOf(array[i]) >= 0) {
         operand += array[i];
+        if (i === array.length - 1) {
+          parsedArray.push(operand);
+        }
       } else if (availableOperators.indexOf(array[i]) >= 0) {
-        if (array[i] === '+' || array[i] === '-') {
-          prevIndex = i;
+        if (i === array.length - 1) {
+          throw new Error('Ошибка ввода. В конце выражения не должно быть знаков - только цифры.');
         } else if (areBadNeighbors(array[i], i, prevIndex)) {
           throw new Error('Ошибка ввода. Не поддерживаются сочетания "+*", "-*, "+/", "-/".');
-        }
-        parsedArray.push(operand); // кладем в массив хранящееся в операнде значение
-        operand = ''; // очищаем переменную операнда
-        parsedArray.push(array[i]); // кладем в массив оператор
-      }
-      if (i === array.length - 1) {
-        if (availableOperators.indexOf(array[i]) >= 0) { // проверяем на ошибки ввода
-          throw new Error('Ошибка ввода. В конце выражения не должно быть знаков - только цифры.');
         } else {
-          parsedArray.push(operand); // добавляем в массив последний из операндов
+          if (isLowerPrecedence(array[i])) {
+            prevIndex = i;
+          }
+          parsedArray.push(operand); // кладем в массив хранящееся в операнде значение
+          operand = ''; // очищаем переменную операнда
+          parsedArray.push(array[i]); // кладем в массив оператор
         }
       }
     }
     if (parsedArray.length > 0) {
-      console.log('parsedArray равен ' + parsedArray);
       return parsedArray;
     } else {
       throw new Error('Ошибка ввода. Вы ввели неподдерживаемый символ или отправили пустую строку.');
@@ -57,7 +66,7 @@
         result = previousOperand / currentOperand;
         break;
       case '+':
-        result = previousOperand + currentOperand;
+        result = Number(previousOperand) + Number(currentOperand);
         break;
       case  '-':
         result = previousOperand - currentOperand;
@@ -67,54 +76,61 @@
     return result;
   };
 
-  const transformToLowPriority = function (array) {
+  const transformToLowPrecedence = function (array) {
     let previousOperand = '';
     let currentOperand = '';
     let operator = '';
-    let operandsAndLowPriorityOperators = [];
+    let lowPrecedenceExpressions = [];
     for (let i = 0; i < array.length; i++) {
-      if (availableOperators.indexOf(array[i])) { // если текущий элемент является оператором
-        if (array[i] === '*' || array[i] === '/') {
-          operator = array[i]; // сохраняем оператор для операции * или /
-        } else {
-          operandsAndLowPriorityOperators.push(previousOperand); // пушим предыдущий операнд в новый массив
-          operandsAndLowPriorityOperators.push(array[i]); // пушим оператор в новый массив
-          previousOperand = ''; // обнуляем предыдущий операнд для следующей операции
-          operator = ''; // очищаем оператор для следующей операции
-        }
-      } else { // если текущий элемент является операндом
-        currentOperand = array[i];
-        if (operator) { // если оператор сохранён, вычисляем произведение или разницу
-          previousOperand = calculateOperands(previousOperand, operator, currentOperand);
-        } else { // если нет сохранённого оператора, то сохраняем текущее значение в предыдущий операнд
+      if (availableOperators.indexOf(array[i]) >= 0) { // если текущий элемент является оператором
+        if (operator !== '') { // если оператор не равен пустой строке
+          lowPrecedenceExpressions.push(previousOperand); // пушим предыдущий операнд в новый массив
+          lowPrecedenceExpressions.push(operator); // пушим предыдущий оператор в новый массив
           previousOperand = currentOperand;
+        }
+        operator = array[i]; // перезаписываем оператор в любом случае
+      } else { // если текущий элемент является операндом (числом)
+        if (previousOperand === '') { // если предыдущий операнд равен пустой строке
+          previousOperand = array[i];
+        } else {
+          if (isLowerPrecedence(operator)) { // если оператор + или -
+            currentOperand = array[i];
+          } else { // если оператор * или /
+            previousOperand = calculateOperands(previousOperand, operator, array[i]).toString();
+            operator = '';
+          }
         }
       }
     }
-    console.log('operandsAndLowPriorityOperators равен ' + operandsAndLowPriorityOperators);
-    return operandsAndLowPriorityOperators;
+    lowPrecedenceExpressions.push(previousOperand);
+    if (operator !== '') {
+      lowPrecedenceExpressions.push(operator);
+      lowPrecedenceExpressions.push(currentOperand);
+    }
+    return lowPrecedenceExpressions;
   };
 
   const calculateResult = function (array) {
-    let operandsAndLowPriorityOperators = transformToLowPriority(array);
+    let lowPrecedenceExpressions = transformToLowPrecedence(array);
+    console.log(lowPrecedenceExpressions);
     let currentOperand = '';
     let previousOperand = '';
     let operator = '';
-    for (let i = 0; i < operandsAndLowPriorityOperators.length; i++) {
-      if (availableOperators.indexOf(array[i])) {
+    for (let i = 0; i < lowPrecedenceExpressions.length; i++) {
+      if (availableOperators.indexOf(array[i]) >= 0) {
         if (operator) {
           previousOperand = calculateOperands(previousOperand, operator, currentOperand);
         }
-        operator = array[i];
+        operator = lowPrecedenceExpressions[i];
       } else {
         if (!operator) {
-          previousOperand = array[i];
+          previousOperand = lowPrecedenceExpressions[i];
         } else {
-          currentOperand = array[i];
+          currentOperand = lowPrecedenceExpressions[i];
         }
       }
     }
-    console.log('previousOperand равен ' + previousOperand);
+    previousOperand = calculateOperands(previousOperand, operator, currentOperand);
     return previousOperand;
   };
 
@@ -127,11 +143,9 @@
     let taskField = document.querySelector('.task-field'); // находим поле для ввода выражения юзером
     const inputtedExpressions = splitToArray(taskField.value); // преобразуем строковое значение поля в массив и сохраняем в переменную
     taskField.value = ''; // очищаем поле для ввода выражения
-    console.log('длина первоначального массива' + inputtedExpressions.length);
     try {
       // разбираем массив введенных значений на операторы и операнды и складываем в новый массив
       const parsedMembers = parseExpressions (inputtedExpressions);
-      console.log('длина разобранного на члены массива' + parsedMembers.length);
       // вызываем функцию, вычисляющую из массива результат
       const result = calculateResult(parsedMembers);
       // вызываем функцию, отображающюю результат
